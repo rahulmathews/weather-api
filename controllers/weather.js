77e27086949c0ab264f190e30911d9991db5333d
Rahul <rahul.mudulakar@smartcontrols.in>
2021-05-02 15:58:15 +0530

    Final Changes

--- controllers/weather.js
+++ controllers/weather.js
@@ -56,33 +56,89 @@ const update = async(req, res, next) => {
 }
 
 /**
- * Updates the User Doc
+ * Compares the latest forecast and sends the similiar pattern 
  * @param {*} req Express request object
  * @param {*} res Express response object
  * @param {*} next Express next callback
 */
 const compare = async(req, res, next) => {
   try{
-    const {email, phone} = _.get(req, 'body');
-    const userId = _.get(req.user , 'userId');
+    
+    let data = await Weather.find({}).lean();
 
-    if(_.isNil(userId)){
-        let error = createError(400, 'User Id is either null or undefined');
-        throw error;
-    };
-      
-    let updateObj = {
-        email: email,
-        phone: phone
-    };
+    let combinationPairs = [];
 
-    await User.updateOne({_id: userId}, {
-        $set: updateObj
-    }, {omitUndefined: true})
+    data.forEach((Obj, index) => {
+      let nextData = data.slice(index + 1);
+
+      nextData.forEach((innerObj) => {
+        let dataObj = {
+          sourceLat: Obj.latitude,
+          sourceLong: Obj.longitude,
+          destLat: innerObj.latitude,
+          destLong: innerObj.longitude,
+        };
+
+        dataObj.changeInTemp = (
+          (
+            Obj.current[Obj.current.length - 1].temp - 
+            innerObj.current[innerObj.current.length - 1].temp
+          ) / 
+          Obj.current[Obj.current.length - 1].temp
+          ) * 100;
+
+        dataObj.changeInFeelsLike = (
+          (
+            Obj.current[Obj.current.length - 1].feels_like - 
+            innerObj.current[innerObj.current.length - 1].feels_like
+          ) / 
+          Obj.current[Obj.current.length - 1].feels_like
+          ) * 100;
+
+        dataObj.changeInPressure = (
+          (
+            Obj.current[Obj.current.length - 1].pressure - 
+            innerObj.current[innerObj.current.length - 1].pressure
+          ) / 
+          Obj.current[Obj.current.length - 1].pressure
+          ) * 100;
+
+        dataObj.changeInHumidity = (
+          (
+            Obj.current[Obj.current.length - 1].humidity - 
+            innerObj.current[innerObj.current.length - 1].humidity
+          ) / 
+          Obj.current[Obj.current.length - 1].humidity
+          ) * 100;
+
+        dataObj.changeInDewPoint = (
+          (
+            Obj.current[Obj.current.length - 1].dew_point - 
+            innerObj.current[innerObj.current.length - 1].dew_point
+          ) / 
+          Obj.current[Obj.current.length - 1].dew_point
+          ) * 100;
+
+        dataObj.changeInWindSpeed = (
+          (
+            Obj.current[Obj.current.length - 1].wind_speed - 
+            innerObj.current[innerObj.current.length - 1].wind_speed
+          ) / 
+          Obj.current[Obj.current.length - 1].wind_speed
+          ) * 100;
+
+        dataObj.resultantChange = Math.abs(dataObj.changeInTemp) + Math.abs(dataObj.changeInFeelsLike) + Math.abs(dataObj.changeInPressure) + Math.abs(dataObj.changeInHumidity) + Math.abs(dataObj.changeInDewPoint) + Math.abs(dataObj.changeInWindSpeed);
+
+        combinationPairs.push(dataObj);
+      })
+    })
+
+    let result = _.minBy(combinationPairs, 'resultantChange');
 
     return res.status(200).json({
         message : 'Success',
-        success: true
+        success: true,
+        result: result
     });
     
   }
--- services/weather.js
+++ services/weather.js
@@ -1,21 +1,10 @@
 const axios = require('axios').default;
 
+const pairs = require('../config/info.json');
+
 
 const getForecast = async(next) => {
     try{
-        const pairs = [
-            [17.385044, 78.486671],
-            [48.856613, 2.352222],
-            [40.712776, -74.005974],
-            [41.878113, -87.629799],
-            [47.751076, -120.740135],
-            [51.5073219, -0.1276474],
-            [35.6828387, 139.7594549],
-            [-33.8548157, 151.2164539],
-            [36.7014631, -118.755997],
-            [37.5666791, 126.9782914]
-        ]
-    
         let api = process.env.WEATHER_API;
     
         let promArr = []
